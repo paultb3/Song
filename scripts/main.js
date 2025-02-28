f91fab464d3b1ca82dcd50cc924a230634eb945d
paultb3 <paulbenancio3000@gmail.com>
2025-02-28 15:14:03 -0500

    configuracion terminada

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,30 @@
-function saludar(){
-    alert('Sos gey amigo')
-}//click, doubleclick, focus.. son eventos de los botones
+const btnXokas = document.querySelector('#btn-xokas');
+const btnAuron = document.querySelector('#btn-auron');
+const btnAstro = document.querySelector('#btn-astro');
+
+const video = document.createElement('video');
+
+const result = document.querySelector('#result');
+
+
+function mostrarVideo(ruta){
+        video.id ='videoElement'
+        video.src = ruta
+        video.controls = true;
+        video.width = 330;
+        video.classList = 'video';
+        video.autoplay = true;
+        result.appendChild(video)
+}
+
+btnXokas.addEventListener('click', function(){
+    mostrarVideo('./assets/videos/xokas.mp4')
+});
+
+btnAuron.addEventListener('click', function(){
+    mostrarVideo('./assets/videos/auron.mp4')
+});
+
+btnAstro.addEventListener('click', function(){
+    mostrarVideo('./assets/videos/astro.mp4')
+});
